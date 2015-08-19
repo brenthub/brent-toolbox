cdbd5675abc8ba8d749ff49232c9a83d0f1ffcba
covito <covito@163.com>
2015-08-19 14:43:43 +0800

    mm

---
+++ src/main/java/cn/brent/toolbox/LoggerHelper.java
@@ -0,0 +1,9 @@
+package cn.brent.toolbox;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerHelper {
+	public static Logger infoLogger=LoggerFactory.getLogger("info");
+	public static Logger debugLogger=LoggerFactory.getLogger("debug");
+}
